8c67850312b779e68f6d54feb29b477b84a9c602
Afsar Tanweer <afsartanweer621@gmail.com>
2026-03-07 20:11:53 +0000

    Getting errow"https://www.mochibazaar.com/admin"

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import Image from "next/image";
 import { 
   Package, 
